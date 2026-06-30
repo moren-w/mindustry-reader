8b142cd61ed2c08c585210dcd499d5966aa8d060
moren-w <2750893539@qq.com>
2026-07-01 02:05:40 +0800

    完善UI

--- script/generater/book.js
+++ script/generater/book.js
@@ -1,22 +1,22 @@
 import { Tiles } from "../schematic/Tiles.js"
 import { Schematic } from "../schematic/Schematic.js"
 
-export function book(text, title) {
+export function book(text, title, blockType) {
   const chunks = splitTextByLength(text);
   const width = Math.ceil(Math.sqrt(chunks.length))
   const height = Math.ceil(chunks.length / width)
   const tiles = new Tiles(width, height)
 
   for (let i = 0; i < chunks.length; i++) {
-    tiles.setTile(i % width, height - 1 - Math.floor(i / width), { n: "message", r: 0, config: chunks[i] })
+    tiles.setTile(i % width, height - 1 - Math.floor(i / width), { n: blockType, r: 0, config: chunks[i] })
   }
 
   const schematicTiles = tiles.toSchematicTiles()
   const tags = new Map()
-  tags.set("name", title)
-  tags.set("description", text.slice(0, 20) + (text.length > 20 ? "..." : ""))
+  tags.set("name", title ? title : "Untitled")
+  tags.set("description", text.slice(0, 50) + (text.length > 50 ? "..." : ""))
   tags.set("contentMap", "{}")
-  tags.set("labels", '["[#ffffffff][#ff69b4ff]moren"]')
+  tags.set("labels", '["114"],["[#ffffffff][#ff69b4ff]moren"]')
   const schematic = new Schematic({
     tiles: schematicTiles,
     height: width,
--- script/index.js
+++ script/index.js
@@ -3,6 +3,8 @@ import { pics } from "../src/background/background.js"
 import { book } from "./generater/book.js"
 import { encodeSchematic } from './encode/encode.js'
 
+let blockType = "message"
+
 function background() {
   const element = $(".background")
   element.css("filter", "blur(4px)")
@@ -17,8 +19,20 @@ setInterval(background, 20 * 1000)
 function generateBook() {
   const title = $("#title").val()
   const text = $("#text").val()
-  const schematic = book(text, title)
+  const schematic = book(text, title, blockType)
   const base64 = encodeSchematic(schematic)
   console.log(base64)
 }
 $("#start").on("click", generateBook)
+
+$("#title, #text, #author").on("keypress", function (e) {
+  if (e.which === 13) {
+    generateBook()
+  }
+})
+
+$(".messages").children("img").on("click", function () {
+  blockType = $(this).attr("alt")
+  $(".messages").children("img").css({"transform": "", "border": "none"})
+  $(this).css({"transform": "scale(1.1)", "border": "4px solid #ffd37f"})
+})
